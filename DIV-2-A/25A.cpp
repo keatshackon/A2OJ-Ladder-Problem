fc8a218b9e442822c5ea10866110281af7b32ff5
keatshackon <keatshackon@gmail.com>
2021-05-16 21:29:05 +0530

    commit

--- DIV-2-A/25A.cpp
+++ DIV-2-A/25A.cpp
@@ -10,32 +10,45 @@ int main() {
 	int i = 1;
 	int ev = 0;
 	int od = 0;
-	int res = 0;
-	std::vector<int> v;
+
+	// vector<int> v;
+
+	int idx1 = 0;
+	int idx2 = 0;
+
 	while (n--) {
 		cin >> q;
 		if (q % 2 == 0) {
 			ev++;
+			idx1 = i ;
 		} else {
 			od++;
+			idx2 = i;
 		}
-		v.push_back(q);
+		i++;
+		// v.push_back(q);
 	}
 
-	if (od == 1) {
-		for (int i = 0; i < v.size(); i++) {
-			if (v[i] % 2 == 1) {
-				cout << i + 1;
-				break;
-			}
-		}
+	if (ev  == 1) {
+		cout << idx1;
 	} else {
-		for (int i = 0; i < v.size(); i++) {
-			if (v[i] % 2 == 0) {
-				cout << i + 1;
-				break;
-			}
-		}
+		cout << idx2;
 	}
+
+	// if (od == 1) {
+	// 	for (int i = 0; i < v.size(); i++) {
+	// 		if (v[i] % 2 == 1) {
+	// 			cout << i + 1;
+	// 			break;
+	// 		}
+	// 	}
+	// } else {
+	// 	for (int i = 0; i < v.size(); i++) {
+	// 		if (v[i] % 2 == 0) {
+	// 			cout << i + 1;
+	// 			break;
+	// 		}
+	// 	}
+	// }
 	return 0;
 }
